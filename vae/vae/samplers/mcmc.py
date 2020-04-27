c88e6451c93fa9feb28526264cecb1b58d221db8
cchadd <clement.chadebec@orange.fr>
2020-04-27 20:05:15 +0200

    add RMHC

--- vae/vae/samplers/mcmc.py
+++ vae/vae/samplers/mcmc.py
@@ -57,6 +57,7 @@ class HMC(BaseMCMCSampler):
         n_lf,
         log_proba,
         M=None,
+        G=None,
         x0=None,
         eps=0.1,
         acc_temp=1,
@@ -82,16 +83,8 @@ class HMC(BaseMCMCSampler):
                                 - if False, returns only the intermediate points
 
         """
-        if x0 is not None:
-            assert isinstance(
-                x0, torch.Tensor
-            ), f"The starting point is not a tensor got {type(x0)} type"
-            assert (
-                x0.size()[-1] == self.dim
-            ), f"Starting point lives in dimension {x0.size()[-1]} while dimension {self.dim} is requested"
 
-        else:
-            x0 = torch.zeros(self.dim)
+        x0 = self.__check_start(x0)        
 
         if M is None:
             M = torch.eye(self.dim)
@@ -107,6 +100,12 @@ class HMC(BaseMCMCSampler):
         # Compute inverse and logdet to avoid repeated computations in the loop
         M_inv = torch.inverse(M)
         M_log_det = torch.logdet(M)
+
+        if G is None:
+            self.sampler_mode = 'HMC'
+
+        else:
+            self.sampler_mode = 'RHMC'
 
         if full_trajectory:
             x = torch.zeros(self.chain_size * n_lf, self.dim)
@@ -126,13 +125,18 @@ class HMC(BaseMCMCSampler):
 
         for i in it:
 
-            x_lf, p_lf = self.__leapfrog(n_lf, log_proba, M, M_inv, x_init)
+            if self.sampler_mode == 'HMC':
+                x_lf, p_lf = self.__leapfrog(n_lf, log_proba, M, M_inv, x_init, eps)
+            
+            else:
+                x_lf, p_lf = self.__generalized_leapfrog(n_lf, log_proba, G, x_init, eps)
+            
             if not skip_acceptance:
                 H_start = self.__hamiltonian(
-                    x_lf[0], p_lf[0], M_inv, M_log_det, log_proba
+                    x_lf[0], p_lf[0], log_proba, M_inv, M_log_det, G=G
                 )
                 H_end = self.__hamiltonian(
-                    x_lf[-1], p_lf[-1], M_inv, M_log_det, log_proba
+                    x_lf[-1], p_lf[-1], log_proba, M_inv, M_log_det, G=G
                 )
 
             if skip_acceptance or torch.rand(1).log() < (-H_end + H_start) / acc_temp:
@@ -157,6 +161,24 @@ class HMC(BaseMCMCSampler):
             print("Acceptance rate: {:03.2f}".format(self.acc / self.chain_size))
 
         return x, p
+
+    def __check_start(self, x0):
+        """
+        Ckeck input type
+        """
+        if x0 is not None:
+            assert isinstance(
+                x0, torch.Tensor
+            ), f"The starting point is not a tensor got {type(x0)} type"
+            assert (
+                x0.size()[-1] == self.dim
+            ), f"Starting point lives in dimension {x0.size()[-1]} while dimension {self.dim} is requested"
+
+        else:
+            x0 = torch.zeros(self.dim)
+
+        return x0
+
 
     def __leapfrog(
         self,
@@ -216,7 +238,83 @@ class HMC(BaseMCMCSampler):
 
         return x, p
 
-    def __hamiltonian(self, x, p, M_inv, M_log_det, log_proba):
+    def __generalized_leapfrog(self, n_lf, log_proba, G, x0, eps=0.1, display_progress=False, only_final=False):
+
+        x = torch.zeros(n_lf + 1, self.dim)
+        p = torch.zeros(n_lf + 1, self.dim)
+        x[0] = x0
+
+        p[0] = torch.distributions.MultivariateNormal(
+            loc=torch.zeros(self.dim), covariance_matrix=G(x0)
+        ).sample()
+        
+        it = (
+            tqdm_notebook(range(1, n_lf + 1))
+            if display_progress
+            else range(1, n_lf + 1)
+        )
+
+        for i in it:
+            x_ = x[i-1].clone()
+            p_ = p[i-1].clone()
+            x_.requires_grad_(True)
+            p_.requires_grad_(True)
+            p_ = self.__leap_step_1(x_, p_, log_proba, G, eps)
+            x_ = self.__leap_step_2(x_, p_, log_proba, G, eps)
+            p_ = self.__leap_step_3(x_, p_, log_proba, G, eps)
+            x[i] = x_.detach()
+            p[i] = p_.detach()
+
+        if only_final:
+            return x[-1], p[-1]
+
+        return x, p
+
+
+    def __leap_step_1(self, x, p, log_proba, G, eps, steps=3):
+        """
+        Resolves eq.16 from Girolami using fixed point iterations
+        """
+        def f_(p):
+            H = self.__hamiltonian(x, p, log_proba, G=G)
+            gx = grad(H, x)[0]
+            return p - 0.5 * eps * gx
+
+        p_ = p.clone()
+        for _ in range(steps):
+            p_ = f_(p_)
+
+        return p_
+
+
+    def __leap_step_2(self, x, p, log_proba, G, eps, steps=3):
+        """
+        Resolves eq.17 from Girolami using fixed point iterations
+        """
+        H0 = self.__hamiltonian(x, p, log_proba, G=G)
+        gp_0 = grad(H0, p)[0]
+
+        def f_(x):
+            H = self.__hamiltonian(x, p, log_proba, G=G)
+            gp = grad(H, p)[0]
+            return x + 0.5 * eps * (gp_0 + gp)
+
+        x_ = x.clone()
+        for _ in range(steps):
+            x_ = f_(x_)
+
+        return x_
+
+    def __leap_step_3(self, x, p, log_proba, G, eps, steps=3):
+        """
+        Resolves eq.18 from Girolami using fixed point iterations
+        """
+        H = self.__hamiltonian(x, p, log_proba, G=G)
+        gx = grad(H, x)[0]
+
+        return p - 0.5 * eps * gx
+
+    def __hamiltonian(self, x, p, log_proba, M_inv=None, M_log_det=None, G=None):
         """
         Computes the Hamiltonian
 
@@ -228,9 +326,26 @@ class HMC(BaseMCMCSampler):
         log_proba (function): function taking [chain_size, dimension] arrays and returns a [chain_size] log_probability
         """
 
-        return (
-            -log_proba(x[None])
-            + 0.5 * M_log_det
-            + 0.5 * p.T @ M_inv @ p
-            + 0.5 * self.dim * torch.Tensor([2 * np.pi]).log()
-        )
+        if self.sampler_mode=='HMC':
+
+            H = (
+                -log_proba(x[None])
+                + 0.5 * M_log_det
+                + 0.5 * p.T @ M_inv @ p
+                + 0.5 * self.dim * torch.Tensor([2 * np.pi]).log()
+            )
+
+        else:
+            G_ = G(x)
+            G_log_det = torch.logdet(G_)
+            G_inv = torch.inverse(G_)
+
+            H = (
+                -log_proba(x[None])
+                + 0.5 * G_log_det
+                + 0.5 * p.T @ G_inv @ p
+                + 0.5 * self.dim * torch.Tensor([2 * np.pi]).log()
+            )
+
+        return H
+            
